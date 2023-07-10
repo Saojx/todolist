d2dacf06d048b61f95282e0ab5823aba6b4640f4
DESKTOP-S1CK78N\Jesse <1516156575@qq.com>
2023-07-10 15:57:06 +0800

    todolist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, INJECTOR, Inject, PLATFORM_ID, inject } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'todoList';
+  title: string = "Add Todo To add a todo, just fill the form below and click in add todo."
+  todo: string = ''
+  todoList: any[] = []
+  showList: any = []
+  todoPage = {
+    page: 1,
+    size: 10
+  }
+  constructor(@Inject(PLATFORM_ID) pid: object, private msg: NzMessageService) { }
+
+  ngOnInit(): void {
+    const list: any = localStorage.getItem('todolist');
+    if (list) {
+      this.todoList = JSON.parse(list);
+      this.updateTodo();
+    }
+  }
+
+  // 添加项
+  addTodo() {
+    if (this.todo.trim() != "") {
+      this.todoList.push(
+        {
+          id: Math.floor(Math.random() * 1000),
+          todo: this.todo,
+          status: 0
+        })
+
+      this.todo = '';
+      this.updateTodo();
+      this.msg.success('Todo added!');
+    }
+  }
+
+  // 删除项
+  delList(id: number) {
+    const index = this.todoList.findIndex(item => item.id === id);
+    if (index != -1) {
+      this.todoList.splice(index, 1);
+      this.msg.warning('Todo removed!');
+    }
+    this.updateTodo();
+  }
+
+  // 分页
+  change(event: any) {
+    if (this.todoPage.page != event) {
+      this.todoPage.page = event;
+      this.updateTodo();
+    }
+  }
+
+  switch() {
+    localStorage.setItem('todolist',JSON.stringify(this.todoList));
+  }
+
+  // 更新数据
+  updateTodo() {
+    this.showList = this.todoList.slice((this.todoPage.page - 1) * this.todoPage.size, this.todoPage.page * this.todoPage.size);
+    localStorage.setItem('todolist',JSON.stringify(this.todoList));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzLayoutModule,
+    NzCardModule,
+    NzInputModule,
+    NzButtonModule,
+    NzListModule,
+    NzPaginationModule,
+    NzTagModule,
+    NzMessageModule,
+    NzSwitchModule,
+    NzIconModule,
+    NzPopconfirmModule
   ],
   providers: [],
   bootstrap: [AppComponent]
