5864e5417cf453bfeee9f7541a3cff4b91f5252e
DESKTOP-S1CK78N\Jesse <1516156575@qq.com>
2023-07-11 10:57:47 +0800

    add table enter click

--- src/app/src/app/pages/table/table.component.ts
+++ src/app/src/app/pages/table/table.component.ts
@@ -9,6 +9,8 @@ export class TableComponent {
   @ViewChildren('inputs') inputs: any = [];
   columns: Array<any> = Array(5).fill('')
   rows: Array<any> = Array(5).fill('')
+  rowIdx: number = 0
+  colIdx: number = 0
   constructor() { }
 
   ngAfterViewInit(): void {
@@ -16,14 +18,26 @@ export class TableComponent {
   }
 
   @HostListener('keyup', ['$event']) keyup(event: any) {
+    if (event.key === 'Enter') {
+      this.focusRight(this.rowIdx, this.colIdx);
+    }
+    // console.log('keyup', this.rowIdx, this.colIdx)
+  }
+
+  focusChange(rowIdx: number, colIdx: number) {
+    this.rowIdx = rowIdx;
+    this.colIdx = colIdx;
   }
 
   focusInput(rowIdx: number, colIdx: number) {
     let inputEls = this.inputs.toArray();
     let flatIdx = (rowIdx * this.columns.length) + colIdx;
-    inputEls[flatIdx].nativeElement.focus();
+    if (flatIdx < (this.columns.length * this.rows.length)) {
+      inputEls[flatIdx].nativeElement.focus();
+    } else {
+      this.focusInput(0, 0)
+    }
   }
-
 
   focusDown(rowIdx: number, colIdx: number) {
     rowIdx = Math.min(rowIdx + 1, this.rows.length - 1);
@@ -42,6 +56,8 @@ export class TableComponent {
 
   focusRight(rowIdx: number, colIdx: number) {
     colIdx = colIdx + 1
+    this.rowIdx = rowIdx;
+    this.colIdx = colIdx;
     this.focusInput(rowIdx, colIdx);
   }
 }
